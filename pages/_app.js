0ccafb9173d45570ae30de77fad87e8102f0f757
demian <demian@kcd.co.kr>
2021-02-06 10:28:53 +0900

    Add ApolloClient to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
+import 'antd/dist/antd.css'
+import { ApolloProvider } from "@apollo/client";
+
+import { useApollo } from "../utils/apolloClient";
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const apolloClient = useApollo(pageProps.initialApolloState);
+
+  return (
+    <ApolloProvider client={apolloClient}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,30 @@
+import { Button, Select, Space } from "antd"
+import { PlusOutlined } from '@ant-design/icons';
+
 export default function Home() {
   return (
-    <div>Home</div>
+    <div style={{ padding: 24 }}>
+      <Space direction="vertical">
+        <Space>
+          <Select
+            showSearch
+            placeholder="Search"
+            filterOption={false}
+            defaultActiveFirstOption={false}
+            onChange={(value, option) => ({})}
+            onSearch={(value) => ({})}
+          >
+            <Select.Option key={0}>Option 1</Select.Option>
+          </Select>
+          <Button
+            type="primary"
+            icon={<PlusOutlined />}
+          >
+            Add Symbol
+          </Button>
+        </Space>
+        <div>List</div>
+      </Space>
+    </div>
   )
 }
